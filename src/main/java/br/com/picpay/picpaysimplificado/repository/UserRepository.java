1683eed4cd8cee508f16c1b53062234f9a8e4606
Pedro Tomaz <p12547254@gmail.com>
2024-10-18 15:43:09 -0300

    feat: create UserRepository

---
+++ src/main/java/br/com/picpay/picpaysimplificado/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.picpay.picpaysimplificado.repository;
+
+import br.com.picpay.picpaysimplificado.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+}
