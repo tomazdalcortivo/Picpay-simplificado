1bebce20cc96dfd4f0738dc39a6148623597f1e9
Pedro Tomaz <p12547254@gmail.com>
2024-11-01 16:26:01 -0300

    feat: TransactionController

---
+++ src/main/java/br/com/picpay/picpaysimplificado/controllers/TransactionController.java
@@ -0,0 +1,27 @@
+package br.com.picpay.picpaysimplificado.controllers;
+
+import br.com.picpay.picpaysimplificado.domain.transaction.Transaction;
+import br.com.picpay.picpaysimplificado.dto.TransactionDTO;
+import br.com.picpay.picpaysimplificado.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transactions")
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService service;
+
+
+    @PostMapping
+    public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
+        Transaction newTransaction = this.service.createTransaction(transaction);
+        return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+}
