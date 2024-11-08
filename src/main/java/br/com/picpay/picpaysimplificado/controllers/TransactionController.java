9f515fc079816d7ee671f8b390b290f015b08db9
Pedro Tomaz <p12547254@gmail.com>
2024-11-08 15:00:16 -0300

    fix: correct Transaction

--- src/main/java/br/com/picpay/picpaysimplificado/controllers/TransactionController.java
+++ src/main/java/br/com/picpay/picpaysimplificado/controllers/TransactionController.java
@@ -11,13 +11,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/transactions")
 @RequiredArgsConstructor
-public class TransactionController {
+public class  TransactionController {
 
     private final TransactionService service;
-
 
     @PostMapping
     public ResponseEntity<Transaction> createTransaction(@RequestBody TransactionDTO transaction) throws Exception {
