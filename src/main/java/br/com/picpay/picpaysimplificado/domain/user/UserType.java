2ad20d40698636fa005e919dd483339873a20d54
Pedro Tomaz <p12547254@gmail.com>
2025-01-06 22:10:06 -0300

    feat: cpf and cnpj and users validations

--- src/main/java/br/com/picpay/picpaysimplificado/domain/user/UserType.java
+++ src/main/java/br/com/picpay/picpaysimplificado/domain/user/UserType.java
@@ -1,6 +1,20 @@
 package br.com.picpay.picpaysimplificado.domain.user;
 
+import br.com.picpay.picpaysimplificado.domain.user.validation.CnpjGroup;
+import br.com.picpay.picpaysimplificado.domain.user.validation.CpfGroup;
+import lombok.Getter;
+
+@Getter
 public enum UserType {
-    COMMON,
-    MERCHANT
+
+    COMMON("CPF", CpfGroup.class),
+    MERCHANT("CNPJ", CnpjGroup.class);
+
+    private final String documentType;
+    private final Class<?> group;
+
+    UserType(String documentType, Class<?> group) {
+        this.documentType = documentType;
+        this.group = group;
+    }
 }
---
+++ src/main/java/br/com/picpay/picpaysimplificado/domain/user/validation/CpfGroup.java
@@ -0,0 +1,4 @@
+package br.com.picpay.picpaysimplificado.domain.user.validation;
+
+public interface CpfGroup {
+}
